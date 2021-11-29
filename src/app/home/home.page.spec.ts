85ccc7859ca19b88377bd3cf24ae7dbd652cc034
Ramneek <ramneekromal@gmail.com>
2021-11-29 19:03:46 +0530

    chess board test completed

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { Color, Direction, HomePage } from './home.page';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
-
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), ReactiveFormsModule, FormsModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
@@ -18,7 +18,104 @@ describe('HomePage', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  fit('should create set place', () => {
     expect(component).toBeTruthy();
+    const form = component.form;
+    const placeButton = getPlaceButton(fixture);
+    expect(placeButton.disabled).toBeTrue();
+    fillInValidForm(form);
+    fixture.detectChanges();
+    expect(placeButton.disabled).toBeFalse();
   });
+  
+  fit('case 1', () => {
+    expect(component).toBeTruthy();
+    const form = component.form;
+    const placeButton = getPlaceButton(fixture);
+    expect(placeButton.disabled).toBeTrue();
+    fillInValidForm(form);
+    fixture.detectChanges();
+    expect(placeButton.disabled).toBeFalse();
+    placeButton.click();
+    fixture.detectChanges();
+
+    const moveButton = getMove1Button(fixture);
+    expect(placeButton.disabled).toBeFalse();
+    moveButton.click();
+    fixture.detectChanges();
+
+
+    report(fixture);
+
+    expect(form.controls.xaxis.value).toEqual(1);
+    expect(form.controls.yaxis.value).toEqual(0);
+    expect(form.controls.direction.value).toEqual(Direction.NORTH);
+    expect(form.controls.color.value).toEqual(Color.WHITE);
+  });
+
+  fit('case 2', () => {
+    expect(component).toBeTruthy();
+    const form = component.form;
+    
+    form.controls.xaxis.setValue(0);
+    form.controls.yaxis.setValue(0);
+    form.controls.direction.setValue(Direction.NORTH);
+    form.controls.color.setValue(Color.BLACK);
+    fixture.detectChanges();
+    expect(form.valid).toBeTrue();
+    const placeButton = getPlaceButton(fixture);
+    fixture.detectChanges();
+    expect(placeButton.disabled).toBeFalse();
+    placeButton.click();
+    fixture.detectChanges();
+
+    const leftButton = getLeftButton(fixture);
+    expect(leftButton).toBeDefined();
+    leftButton.click();
+    fixture.detectChanges();
+
+
+    report(fixture);
+
+    expect(form.controls.xaxis.value).toEqual(0);
+    expect(form.controls.yaxis.value).toEqual(0);
+    expect(form.controls.direction.value).toEqual(Direction.WEST);
+    expect(form.controls.color.value).toEqual(Color.BLACK);
+  });
+
+  function getPlaceButton(fixture: ComponentFixture<HomePage>) {
+    return fixture.nativeElement.querySelector('.place-button');
+  }
+
+  function getMove1Button(fixture: ComponentFixture<HomePage>) {
+    return fixture.nativeElement.querySelector('.move1');
+  }
+
+  function getMove2Button(fixture: ComponentFixture<HomePage>) {
+    return fixture.nativeElement.querySelector('.move2');
+  }
+
+  function report(fixture: ComponentFixture<HomePage>) {
+    var reportButton =  fixture.nativeElement.querySelector('.report');
+    expect(reportButton).toBeDefined();
+    reportButton.click();
+    fixture.detectChanges();
+  }
+  function getLeftButton(fixture: ComponentFixture<HomePage>) {
+    return fixture.nativeElement.querySelector('.left');
+  }
+
+  function getRightButton(fixture: ComponentFixture<HomePage>) {
+    return fixture.nativeElement.querySelector('.right');
+  }
+
+
+  function fillInValidForm(form: FormGroup) {
+    form.controls.xaxis.setValue(0);
+    form.controls.yaxis.setValue(0);
+    form.controls.direction.setValue(Direction.NORTH);
+    form.controls.color.setValue(Color.WHITE);
+    fixture.detectChanges();
+    expect(form.valid).toBeTrue();
+  }
 });
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
-
-enum Direction {
-  NORTH, EAST, WEST, SOUTH
-}
-
-enum Color {
+export enum Color {
   WHITE, BLACK
 }
 
-interface Place {
+export interface Place {
   x: number;
   y: number;
   direction: Direction;
@@ -18,6 +14,37 @@ interface Place {
 }
 
 
+export class Direction {
+  private static currentIndex = 0;
+  private static values: Direction[] = [];
+  private readonly index: number;
+
+  private constructor() {
+    this.index = Direction.currentIndex++;
+
+    Direction.values.push(this);
+  }
+
+  static readonly NORTH = new Direction();
+  static readonly EAST = new Direction();
+  static readonly SOUTH = new Direction();
+  static readonly WEST = new Direction();
+
+  get previous() {
+    const previous: Direction = Direction.WEST;
+    if (this.index > 0) {
+      return Direction.values[this.index - 1];
+    }
+    return previous;
+  }
+  get next() {
+    const previous: Direction = Direction.NORTH;
+    if (this.index !== 3) {
+      return Direction.values[this.index + 1];
+    }
+    return previous;
+  }
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -26,29 +53,58 @@ interface Place {
 export class HomePage {
   public Color = Color;
   public Direction = Direction;
-  public currentPlace: Place = {
-      x: 0,
-      y: 0,
-      direction: Direction.NORTH,
-      color: Color.WHITE,
-  }
+  public black = true;
+  public white = true;
+  public currentPlace: Place = null
+  public report: Place = null
   form = this.formBuilder.group({
-    xaxis: this.formBuilder.control(this.currentPlace.x,[Validators.required]),
-    yaxis: this.formBuilder.control(this.currentPlace.y,[Validators.required]),
-    direction: this.formBuilder.control(this.currentPlace.direction,[Validators.required]),
-    color: this.formBuilder.control(this.currentPlace.color,[Validators.required])
+    xaxis: this.formBuilder.control(0,[Validators.required]),
+    yaxis: this.formBuilder.control(0,[Validators.required]),
+    direction: this.formBuilder.control( '',[Validators.required]),
+    color: this.formBuilder.control('',[Validators.required])
   })
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private alertController: AlertController
   ) {}
 
   submit(){
-    console.log("form = ", this.form.value)
     var formValues = this.form.value
-    this.currentPlace.x = formValues.xaxis
-    this.currentPlace.y = formValues.yaxis
-    this.currentPlace.direction = formValues.direction
-    this.currentPlace.color = formValues.color
+    this.currentPlace = {
+      x: formValues.xaxis,
+      y: formValues.yaxis,
+      direction: formValues.direction,
+      color: formValues.color,
+    }
+  }
+  move(steps){
+    var {direction,x,y} = this.currentPlace
+    if(direction == Direction.NORTH && (7 - x) >= steps){
+      this.currentPlace.x = x + steps;
+    }else if(direction == Direction.EAST && (7 - y) >= steps  ){
+      this.currentPlace.y = y + steps;
+    }else if(direction == Direction.SOUTH && (x - steps) >= 0 ){
+      this.currentPlace.x = x - steps;
+    }else if(direction == Direction.WEST && (y - steps) >= 0){
+      this.currentPlace.y = y - steps;
+    }else{
+      this.throwError();
+    }
+  }
+  setReport(){
+    this.form.controls.xaxis.setValue(this.currentPlace.x)
+    this.form.controls.yaxis.setValue(this.currentPlace.y)
+    this.form.controls.direction.setValue(this.currentPlace.direction)
+    this.form.controls.color.setValue(this.currentPlace.color)
   }
 
+  async throwError() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'This is not valid command, you are on edge of board',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
